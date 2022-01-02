4198ac444137dfbfa1876c19ce29b6545afab3d7
Liam Cornu <71927979+Inkapable@users.noreply.github.com>
2022-01-02 02:18:02 +0100

    Ajout du service de recherche de participation de l'utilisateur actif

--- frontend/src/services/participations.service.js
+++ frontend/src/services/participations.service.js
@@ -2,6 +2,23 @@ const API_URL = "http://157.90.237.150/api/v1/";
 
 
 class GestionParticipations{
+
+
+    async getActiveUserParticipations(participationRequestInfo){
+        const headers = {"Authorization": "Bearer " + localStorage.getItem('token')};
+        const requestOptions = {
+            headers: headers,
+            method: 'GET',
+        }
+        const response = await fetch(API_URL + 'participation/me?' + new URLSearchParams(participationRequestInfo).toString(), requestOptions);
+        const data = await response.json();
+        if (!response.ok){
+            const error = (data) || response.status;
+            return Promise.reject(error.detail);
+        }
+        return data
+    }
+
     //TO DO:
     async addMyParticipation(requestInfo) {
         const headers = {"Authorization": "Bearer " + localStorage.getItem('token')};
